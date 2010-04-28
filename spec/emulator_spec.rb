37db0362a9b5c1ea280358eb36ddd49451d75e81
Carlos Beltrán-Recabarren <cbrecabarren@gmail.com>
2010-04-28 12:41:51 -0400

    added some examples to specs

--- spec/emulator_spec.rb
+++ spec/emulator_spec.rb
@@ -3,11 +3,23 @@ require 'spec_helper'
 include GME
 
 describe Emulator, "when first created" do
-  it "should do this" do 
-    song = Emulator.open(File.expand_path(File.dirname(__FILE__) + '/fixtures/sample.spc'))
-
-    song.should_not be_nil
+  before(:all) do
+    path = File.expand_path(File.dirname(__FILE__) + '/fixtures/sample.spc')
+    @song = Emulator.open(path)
+  end
+  it "should not be nil" do 
+    @song.should_not be_nil
   end
   
-  it "should do something"
+  it "should contain 1 track" do 
+    @song.track_count.should == 1
+  end
+
+  it "should load the metadata in a container and respond to the message 'info'" do 
+    @song.info.should_not be_empty
+  end
+
+  it "should indicate that no track has been started" do 
+    @song.track_started?.should be_false
+  end
 end
